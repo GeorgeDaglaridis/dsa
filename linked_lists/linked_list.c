7649190c2a8fbf319b8b9f9e6f3880b7cf87eeb4
George Theodoros P. Daglaridis <george_daglaridis@yahoo.com>
2026-04-09 11:53:07 +0300

    create add_in_the_beginning and traverse functions
    According to our current strategy we add every new node in the
    beginning, before the last created node

--- linked_lists/linked_list.c
+++ linked_lists/linked_list.c
@@ -3,13 +3,14 @@
 #include <string.h>
 
 int init(char *data);
+int add_in_the_beginning(char *data);
+void traverse(void);
 
 typedef struct node {
     char data[40];
     struct node *next;
 
 } node_s;
-
 
 node_s *__head = NULL;
 
@@ -19,23 +20,27 @@ int main(void) {
     // printf("sizeof(arr) = %ld\n", sizeof(arr));
 
     init("George");
-    init("Panos");
+
+    add_in_the_beginning("Panos");
+    add_in_the_beginning("Stella");
+
+    traverse();
 
     return 0;
 }
 
 int init(char *data) {
     if(__head) {
-        printf("Initialization has happened!\n");
-        return -2;
+        printf("Linked list already initialized!\n");
+        return -1;
     }
 
     printf("sizeof(node_s) = %ld\n", sizeof(node_s));
 
     __head = (node_s *) malloc(sizeof(node_s));
     if(!__head) {
-        printf("Memory allocation failed!\n");
-        return -1;
+        printf("Init memory allocation failed!\n");
+        return -2;
     }
 
     // printf("strlen(data) = %ld\n", strlen(data));
@@ -61,6 +66,51 @@ int init(char *data) {
     printf("address of __head->data = %p\n", &__head->data);
 
 
-    free(__head);
+    //free(__head);
     return 0;
+}
+
+int add_in_the_beginning(char *data) {
+    if(!data) {
+        printf("Invalid data!\n");
+        return -1;
+    }
+
+    printf("\n\n");
+
+    node_s *n = (node_s *) malloc(sizeof(node_s));
+    if(!n) {
+        printf("New node memory allocation failed!\n");
+        return -2;
+    }
+
+    printf("address new node n points to: %p\n", n);
+    printf("address of n->data: %p\n", &n->data);
+    
+    // Link the new node with the previous one.
+    // Practically put the new node before the one you
+    // created before.
+    n->next = __head;
+    printf("address n->next points to: %p\n", n->next);
+
+    memcpy(n->data, data, strlen(data));
+    printf("n->data = %s\n", n->data);
+
+    __head = n;
+    printf("address new __head points to: %p\n", __head);
+    
+    return 0;
+}
+
+void traverse(void) {
+    printf("\n\n");
+
+    int i = 0;
+    for(node_s *cursor = __head; cursor != NULL; cursor = cursor->next) {
+        // According to the current strategy we have followed
+        // for adding new nodes, the last added node is printed
+        // first
+        printf("Node[%d]: %s\n", i, cursor->data);
+        i++;
+    }
 }
