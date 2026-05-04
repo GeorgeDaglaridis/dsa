a75b3bdc2a957c1d26b1aeb3b1873381571411d1
George Theodoros P. Daglaridis <george_daglaridis@yahoo.com>
2026-05-04 12:08:34 +0300

    starting scheme for creating and destroying a list container

--- linked_lists/sll_upd3.c
+++ linked_lists/sll_upd3.c
@@ -8,7 +8,8 @@ typedef enum {
     LIST_ALRD_INIT = -2,
     LIST_ERR_ALLOC = -3,
     LIST_INV_DATA = -4,
-    LIST_KEY_NOT_FOUND = -5
+    LIST_KEY_NOT_FOUND = -5,
+    LIST_NOT_FREED = -6
 } list_status;
 
 // Represents each node of the linked list
@@ -17,13 +18,54 @@ typedef struct node {
     struct node *next_node;
 } node_s;
 
-// Manages the list that manages nodes
+// List container: Manages the list that manages nodes
 typedef struct list {
     node_s *head;
     node_s *tail;
     size_t size;
 } list_s;
 
-int main() {
+list_s *list_create(void) {
+    
+    list_s *ll = calloc(1, sizeof(*ll));
+    if(!ll) {
+        return NULL;
+    }
+
+    return ll;
+}
+
+// Incomplete: Make sure you free all nodes first before you free
+// the list container
+list_status list_destroy(list_s **ll) {
+    // Defensive check: Do not trust the arguments 
+    // user provides you. Check for all cases.
+    if (ll == NULL) {
+        return LIST_NOT_FREED;
+    }
+    else if (*ll == NULL) {
+        return LIST_NOT_FREED;
+    }
+    else {
+        free(*ll);
+        *ll = NULL;
+        return 0;
+    }
+}
+
+int main(void) {
+
+    list_s *sll1 = list_create();
+    if(!sll1) {
+        printf("Memory allocation failed!\n");
+        return LIST_ERR_ALLOC;
+    }
+
+    printf("Before freeing: address sll1 points to: %p\n", sll1);
+    // The following format is called ternary expression
+    (list_destroy(&sll1) == LIST_NOT_FREED) ? printf("Not freed ... !\n") : printf("Freed ... !\n");
+    printf("After freeing: address sll1 points to: %p\n", sll1);
+    
     return 0;
 }
+
